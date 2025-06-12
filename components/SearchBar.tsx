4c2a119d8a87ceb0992f33633c8cf1be4aacf2eb
Shijin2k3 <shijinkumar2003@gmail.com>
2025-06-12 15:11:17 +0530

    initialized first commit

---
+++ components/SearchBar.tsx
@@ -0,0 +1,61 @@
+// import { icons } from '@/constants/icons';
+// import React from 'react';
+// import { Image, TextInput, View } from 'react-native';
+
+// interface Props {
+//     placeholder: string;
+//     onPress?: () => void;
+//     value?: string;
+//     onChangeText?: (text: string) => void;
+//     editable?: boolean;
+//     ref: any;
+// }
+
+// const SearchBar = ({ placeholder, onPress, value, onChangeText, editable = true, ref }: Props) => {
+//     return (
+//         <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+//             <Image source={icons.search} className='size-5' resizeMode='contain' tintColor='#ab8bff' />
+//             <TextInput
+//                 ref={ref}
+//                 onPress={onPress}
+//                 placeholder={placeholder}
+//                 value={value}
+//                 onChangeText={onChangeText}
+//                 placeholderTextColor='#a8b5db'
+//                 className='flex-1 ml-2 text-white'
+//                 editable={editable}
+//                 autoFocus={false}
+//             />
+//         </View>
+//     );
+// };
+
+// export default SearchBar;
+import { View, Text, Image, TextInput } from 'react-native'
+import React from 'react'
+import { icons } from '@/constants/icons'
+
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+//   value?: string;
+//   onChangeText?: (text: string) => void;
+//   editable?: boolean;
+}
+
+const SearchBar = ({placeholder,onPress}:Props) => {
+  return (
+    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+      <Image source={icons.search} className='size-5' resizeMode='contain' tintColor='#ab8bff' />
+      <TextInput 
+       onPress={onPress}
+       placeholder='Search for a movie'
+       value=''
+       onChangeText={()=>{}}
+       placeholderTextColor={placeholder} 
+       className='flex-1 ml-2 text-white'/>
+    </View>
+  )
+}
+
+export default SearchBar
